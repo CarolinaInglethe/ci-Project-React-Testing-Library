eeb98d16a171b2fe0ada8bea4fa48bca575d8a4a
Carol_Inglethe <carolinainglethe@gmail.com>
2021-12-04 18:21:55 -0300

    Updates

---
+++ precache-manifest.ba947793ca91b1fed63ed324311c0dac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989996d5ccfab642f4af25d3370927a4",
+    "url": "/ci-Project-React-Testing-Library/index.html"
+  },
+  {
+    "revision": "caec96e754fbb9eb9ae5",
+    "url": "/ci-Project-React-Testing-Library/static/css/main.88a5c871.chunk.css"
+  },
+  {
+    "revision": "760c68df0d5e85e1e9f9",
+    "url": "/ci-Project-React-Testing-Library/static/js/2.01afe734.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ci-Project-React-Testing-Library/static/js/2.01afe734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caec96e754fbb9eb9ae5",
+    "url": "/ci-Project-React-Testing-Library/static/js/main.74fba4a2.chunk.js"
+  },
+  {
+    "revision": "2890dfcc5a34207a184c",
+    "url": "/ci-Project-React-Testing-Library/static/js/runtime-main.9d9043de.js"
+  }
+]);
